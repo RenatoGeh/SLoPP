adfd67b8bded1b7ed733bd0faaab3b19cdeb122a
Renato Lui Geh <renatogeh@gmail.com>
2023-06-25 16:59:49 -0300

    Fixes to SLoPP implementation

--- src/SLoPP.jl
+++ src/SLoPP.jl
@@ -6,6 +6,17 @@ using LogicCircuits
 using ProbabilisticCircuits
 using Random
 using BlossomV
+using Metis
+
+function factorize(D::AbstractDataFrame, V::Vtree)::StructProbCircuit
+  if isleaf(V)
+    X = Lit(variable(V))
+    if D[1, 1] return StructProbLiteralNode(X, V) end
+    return StructProbLiteralNode(-X, V)
+  end
+  return StructMulNode(factorize(view(D, :, Symbol.(variables(V.left))), V.left),
+                       factorize(view(D, :, Symbol.(variables(V.right))), V.right), V)
+end
 
 function _slopp(D::AbstractDataFrame, V::Vtree, k::Integer)::StructProbCircuit
   n, m = size(D)
@@ -17,20 +28,25 @@ function _slopp(D::AbstractDataFrame, V::Vtree, k::Integer)::StructProbCircuit
     # Positive literal.
     if z == n return StructProbLiteralNode(X, V)
     # Negative literal.
-    elseif z == 0 return StructProbLiteralNode(-X, V)
+    elseif z == 0 return StructProbLiteralNode(-X, V) end
     # Bernoulli.
-    else
-      w = z/n
-      return StructSumNode([StructProbLiteralNode(X, V), StructProbLiteralNode(-X, V)],
-                           log.([w, 1-w]), V)
-    end
-  end
+    w = z/n
+    return StructSumNode([StructProbLiteralNode(X, V), StructProbLiteralNode(-X, V)],
+                         log.([w, 1-w]), V)
+  # If there is only one example, consider it as a conjunction over all assignments.
+  elseif n == 1 return factorize(D, V) end
   # Cluster.
   R = kmeans(transpose(Matrix(D)), k; display = :none)
+  # If all instances are the same, return a conjunction over all assignments.
+  if any(x -> x == 0, R.counts) return factorize(D, V) end
   # Get indices.
   I = Vector{Vector{Int}}(undef, k)
-  for i in 1:k I[i] = Vector{Int}() end
-  for i in 1:n push!(I[R.assignments[i]], i) end
+  incr = zeros(UInt32, k)
+  for i in 1:k I[i] = Vector{Int}(undef, R.counts[i]) end
+  for i in 1:n
+    j = R.assignments[i]
+    I[j][incr[j] += 1] = i
+  end
   # Elements (conjunction of prime and sub).
   Ch = Vector{StructMulNode}(undef, k)
   # Recurse over clusters.
@@ -57,8 +73,26 @@ function slopp(D::AbstractDataFrame, k::Integer; V::Union{Vtree, Nothing} = noth
 end
 export slopp
 
-function evaluate(name::String, k::Integer; seed::Integer = 0, alg::Symbol = :topdown,
-    param::Symbol = :none, args...)::Tuple{Float64, Float64, Float64, StructProbCircuit}
+function revise!(D::AbstractDataFrame, k::Integer, C::StructProbCircuit, n_original::Int;
+    seed::Integer = 0, alg::Symbol = :topdown)
+  if seed >= 0 Random.seed!(seed) end
+  rename!(D, Symbol.(1:size(D, 2)))
+  D_v = view(D, findall(isinf, log_likelihood_per_instance(C, D)), :)
+  if size(D_v, 1) == 0 return C end
+  Z = _slopp(D_v, vtree(C), k)
+  n = size(D_v, 1)
+  N = n+n_original
+  proportions = Vector{Float64}(undef, length(C.children)+length(Z.children))
+  proportions[1:length(C.children)] .= (n_original .* exp.(C.log_probs)) ./ N
+  proportions[length(C.children)+1:end] .= (n .* exp.(Z.log_probs)) ./ N
+  C.log_probs = log.(proportions)
+  append!(C.children, Z.children)
+  return C
+end
+export revise!
+
+function slopp_from_data(name::String, k::Integer; seed::Integer = 0, alg::Symbol = :topdown,
+    param::Symbol = :none, args...)::StructProbCircuit
   print("Pulling dataset...")
   train, valid, test = twenty_datasets(name)
   rename!(train, Symbol.(1:size(train, 2)))
@@ -71,13 +105,31 @@ function evaluate(name::String, k::Integer; seed::Integer = 0, alg::Symbol = :to
     elseif param == :sgd estimate_parameters_sgd!(C, train; args...) end
     println(" OK!")
   end
-  print("Evaluating train set...")
-  ll_train = log_likelihood_avg(C, train)
-  print(" OK!\nEvaluating valid set...")
-  ll_valid = log_likelihood_avg(C, valid)
-  print(" OK!\nEvaluating test set...")
-  ll_test = log_likelihood_avg(C, test)
+  return C
+end
+export slopp_from_data
+
+function evaluate(name::String, C::StructProbCircuit; ignore::Bool = true)::Tuple{Float64, Float64, Float64}
+  print("Pulling dataset...")
+  train, valid, test = twenty_datasets(name)
+  rename!(train, Symbol.(1:size(train, 2)))
+  ev(circ, data) = !ignore ? log_likelihood_avg(circ, data) :
+                             sum(filter(x -> !isinf(x),
+                                        log_likelihood_per_instance(circ, data)))/size(data, 1)
   println(" OK!")
+  print("Evaluating train set...")
+  ll_train = ev(C, train)
+  print(" OK!\nEvaluating valid set...")
+  ll_valid = ev(C, valid)
+  print(" OK!\nEvaluating test set...")
+  ll_test = ev(C, test)
+  println(" OK!")
+  return ll_train, ll_valid, ll_test
+end
+function evaluate(name::String, k::Integer; seed::Integer = 0, alg::Symbol = :topdown,
+    param::Symbol = :none, ignore::Bool = true, args...)::Tuple{Float64, Float64, Float64, StructProbCircuit}
+  C = slopp_from_data(name, k; seed, alg, param, args...)
+  ll_train, ll_valid, ll_test = evaluate(name, C; ignore)
   return ll_train, ll_valid, ll_test, C
 end
 export evaluate
